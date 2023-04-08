b05954856c527fd111c28e1f80f384a69f067bdc
jovi <jovisimons009@gmail.com>
2023-04-08 17:45:40 +0200

    ultimate bimbo sem

--- src/main/java/jovisimons/dekeet/UserService/controller/UserController.java
+++ src/main/java/jovisimons/dekeet/UserService/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
 @CrossOrigin(origins = "http://localhost:3000")
 public class UserController {
     @Autowired
