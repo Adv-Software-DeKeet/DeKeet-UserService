ea6638d05df77af7f8d04bbd1089ef424d7ae38b
jovi <jovisimons009@gmail.com>
2023-05-10 00:23:00 +0200

    mock rabbit

--- src/test/java/jovisimons/dekeet/UserService/BaseClass.java
+++ src/test/java/jovisimons/dekeet/UserService/BaseClass.java
@@ -1,9 +1,13 @@
 package jovisimons.dekeet.UserService;
 
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest(
         properties = "de.flapdoodle.mongodb.embedded.version=5.0.5"
 )
 public class BaseClass {
+    @MockBean
+    RabbitTemplate rabbitTemplate;
 }
--- src/test/java/jovisimons/dekeet/UserService/userIntegrationTest/UserControllerTest.java
+++ src/test/java/jovisimons/dekeet/UserService/userIntegrationTest/UserControllerTest.java
@@ -6,8 +6,10 @@ import jovisimons.dekeet.common.model.User;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
